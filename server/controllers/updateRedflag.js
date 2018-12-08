869efcaafb647b6883e77d9b18a7f2da6db0bd95
fejiroofficial <houseofjiro@gmail.com>
2018-12-09 00:24:05 +0100

    feat: Update red-flag location This is a commit for an endpoint which allows a user to update the location of a red-flag record.
    [ Delivers #162528243 ]

--- server/controllers/updateRedflag.js
+++ server/controllers/updateRedflag.js
@@ -77,6 +77,76 @@ class UpdateRedFlagController {
         });
       }));
   }
+
+  /**
+* @function updateLocation
+* @memberof UpdateRedFlagController
+* @static
+*/
+  static updateLocation(req, res) {
+    const redFlagId = parseInt(req.params.id, 10);
+    const { userId } = req;
+    let { latitude, longitude } = req.body;
+    latitude = latitude ? latitude.toString().replace(/\s+/g, '') : latitude;
+    longitude = longitude ? longitude.toString().replace(/\s+/g, '') : longitude;
+    const location = `${latitude},${longitude}`;
+    if (isNaN(redFlagId)) {
+      return res.status(400).json({
+        success: 'false',
+        message: 'param should be a number not an alphabet',
+      });
+    }
+    return db.task('find red-flag', data => data.incidents.findById(redFlagId)
+      .then((record) => {
+        if (!record) {
+          return res.status(404).json({
+            status: 404,
+            success: 'false',
+            message: 'This record doesn\'t exist in the database',
+          });
+        }
+        if (record.type !== 'red-flag') {
+          return res.status(400).json({
+            status: 400,
+            success: 'false',
+            message: 'This incident record is not a red-flag',
+          });
+        }
+        const recordOwner = userId === record.createdby;
+        if (!recordOwner) {
+          return res.status(401).json({
+            status: 401,
+            success: 'false',
+            message: 'You are unauthorized to update an information that was not posted by you',
+          });
+        }
+        if (record.status !== 'draft') {
+          return res.status(403).json({
+            status: 403,
+            success: 'false',
+            message: `This record is marked as ${record.status}. Updating location is no longer allowed`,
+          });
+        }
+        return db.task('update location', data => data.incidents.modifyLocation({ location }, redFlagId)
+          .then(() => {
+            return res.status(200).json({
+              status: 200,
+              success: 'true',
+              data: [{
+                id: redFlagId,
+                message: 'You have successfully updated the location of this red-flag record',
+              }],
+            });
+          }));
+      })
+      .catch((err) => {
+        return res.status(500).json({
+          success: 'false',
+          message: 'so sorry, something went wrong, try again',
+          err: err.message,
+        });
+      }));
+  }
 }
 
 export default UpdateRedFlagController;
